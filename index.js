21cd284fc51037bdbed5752ed310cb8c7a7f5f28
esteban <estebanfernandez20003024@gmail.com>
2022-08-31 12:02:27 -0300

    articulo 3

---
+++ index.js
@@ -0,0 +1,12 @@
+const gmail = document.getElementById("gmail")
+const mail = document.getElementById("mail") 
+const x = document.getElementById("equis") 
+
+
+gmail.onclick = (e) => {
+mail.style.display = "flex"
+}
+
+x.onclick = (e) => {
+    mail.style.display = "none"
+}
